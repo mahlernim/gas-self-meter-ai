1ddb81c7cdd54e707695f2bf98192b96a82b7ef1
mahler83 <33252024+SangzinAhn@users.noreply.github.com>
2026-09-04 01:18:38 +0900

    Prepare Closed Alpha 0.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,8 +9,8 @@ android {
         applicationId = "dev.mahlernim.gasselfmeter"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
-        versionName = "0.2.0"
+        versionCode = 4
+        versionName = "0.2.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     buildFeatures { compose = true; buildConfig = true }
